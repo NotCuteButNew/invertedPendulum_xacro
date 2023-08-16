6e67dfdc689790ee0d13a676497fe7b8112f7fca
NotCuteButNew <bwd2386884119@gmail.com>
2023-08-16 23:23:02 +0800

    update kp ki kd

--- src/robot_control/include/robot_control/config.h
+++ src/robot_control/include/robot_control/config.h
@@ -2,19 +2,19 @@
 #define CONFIG_H
 
 
-#define Kp_rad 35
-#define Ki_rad 0.05
-#define Kd_rad 6
+#define Kp_rad -37*0.8//-30
+#define Ki_rad 0
+#define Kd_rad -6.5*0.8
 
-#define Kp_pos 10
-#define Ki_pos 0
-#define Kd_pos 0
+#define Kp_vel 10
+#define Ki_vel 0
+#define Kd_vel 0
 
 
 #define pub_rate 50
 
 //limit
-#define min_rad -0.33
-#define max_rad 0.33
+#define min_rad -0.38
+#define max_rad 0.38
 
 #endif
