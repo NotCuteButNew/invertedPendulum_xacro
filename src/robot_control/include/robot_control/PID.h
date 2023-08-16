18eaa2b6d26e3debc2bf150c1d0a9f831b5af7b6
NotCuteButNew <bwd2386884119@gmail.com>
2023-08-16 23:40:12 +0800

    add limit

--- src/robot_control/include/robot_control/PID.h
+++ src/robot_control/include/robot_control/PID.h
@@ -15,10 +15,14 @@ private:
     float e;//误差
     float e_pre;//上一次误差
     float integral;//积分项
+    float limit_max;
+    float limit_min;
+    bool enable_limit;
 public:
     PID_position();
     ~PID_position(){};
     PID_position(float p,float i,float d);
+    void limit(float min,float max,bool enable_limit);
     float pid_control(float tar,float act);//执行PID控制
     void pid_show();//显示PID控制器的内部参数
 };
