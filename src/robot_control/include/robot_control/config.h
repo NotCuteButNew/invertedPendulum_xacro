4678c8c1a0551cdd316a09e599df4711ff860159
NotCuteButNew <bwd2386884119@gmail.com>
2023-08-16 17:04:05 +0800

    add some configuration

---
+++ src/robot_control/include/robot_control/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+#define Kp_rad 35
+#define Ki_rad 0.05
+#define Kd_rad 6
+
+#define Kp_pos 10
+#define Ki_pos 0
+#define Kd_pos 0
+
+
+#define pub_rate 50
+
+//limit
+#define min_rad -0.33
+#define max_rad 0.33
+
+#endif
