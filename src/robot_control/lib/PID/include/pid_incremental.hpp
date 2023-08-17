02b9b92d30d959065a73eddd6cfad9bae407b31d
NotCuteButNew <bwd2386884119@gmail.com>
2023-08-17 15:08:01 +0800

    feat:add incremental pid

---
+++ src/robot_control/lib/PID/include/pid_incremental.hpp
@@ -0,0 +1,61 @@
+#ifndef PID_PID_INCREMENTAL_HPP_
+#define PID_PID_INCREMENTAL_HPP_
+
+#include "pid_interface.hpp"
+
+namespace pid {
+template <typename T> class IncrementalPid : public PidInterface<T> {
+private:
+  T kp_;
+  T ki_;
+  T kd_;
+  T target_;
+  T actual_;
+  T e_;
+  T last_e_;
+  T past_past_e_;
+  T u_;
+
+public:
+  IncrementalPid<T>()
+      : kp_(0), ki_(0), kd_(0), e_(0), last_e_(0), past_past_e_(0), u_(0) {}
+  IncrementalPid<T>(T p, T i, T d)
+      : kp_(p), ki_(i), kd_(d), e_(0), last_e_(0), past_past_e_(0), u_(0) {}
+  inline bool SetPid(const T p, const T i, const T d) override;
+  inline T Proportional() override;
+  inline T Integral() override;
+  inline T Derivative() override;
+  T Calculate(const T target, const T actual) override;
+  void Show() override;
+};
+
+template <typename T>
+inline bool IncrementalPid<T>::SetPid(const T p, const T i, const T d) {
+  kp_ = p;
+  ki_ = i;
+  kd_ = d;
+  return true;
+}
+
+template <typename T> T IncrementalPid<T>::Proportional() {
+  return e_ - last_e_;
+}
+
+template <typename T> T IncrementalPid<T>::Integral() { return e_; }
+
+template <typename T> T IncrementalPid<T>::Derivative() {
+  return e_ - 2 * last_e_ + past_past_e_;
+}
+
+template <typename T>
+T IncrementalPid<T>::Calculate(const T target, const T actual) {
+  e_ = target - actual;
+  u_ = kp_ * Proportional() + ki_ * Integral() + kd_ * Derivative();
+  past_past_e_ = last_e_;
+  last_e_ = e_;
+  return u_;
+}
+
+} // namespace pid
+
+#endif
--- src/robot_control/lib/PID/include/pid_position.hpp
+++ src/robot_control/lib/PID/include/pid_position.hpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 
 namespace pid {
-template <typename T> class PidPosition : public PidInterface<T> {
+template <typename T> class PositionalPid : public PidInterface<T> {
 private:
   T kp_;
   T ki_;
@@ -21,10 +21,10 @@ private:
   T u_;
 
 public:
-  PidPosition<T>()
+  PositionalPid<T>()
       : kp_(0), ki_(0), kd_(0), enable_limit_(false), limit_min_(0),
         limit_max_(0), e_(0), last_e_(0), integral_0_t_(0), u_(0) {}
-  PidPosition<T>(T p, T i, T d, bool enable = false, T min = 0, T max = 0)
+  PositionalPid<T>(T p, T i, T d, bool enable = false, T min = 0, T max = 0)
       : kp_(p), ki_(i), kd_(d), enable_limit_(enable), limit_min_(min),
         limit_max_(max), e_(0), last_e_(0), integral_0_t_(0), u_(0) {}
   inline bool SetPid(const T p, const T i, const T d) override;
@@ -38,7 +38,7 @@ public:
 };
 
 template <typename T>
-inline bool PidPosition<T>::SetPid(const T p, const T i, const T d) {
+inline bool PositionalPid<T>::SetPid(const T p, const T i, const T d) {
   kp_ = p;
   ki_ = i;
   kd_ = d;
@@ -46,36 +46,33 @@ inline bool PidPosition<T>::SetPid(const T p, const T i, const T d) {
 }
 
 template <typename T>
-inline bool PidPosition<T>::SetLimiterStatus(bool status) {
+inline bool PositionalPid<T>::SetLimiterStatus(bool status) {
   enable_limit_ = status;
   return enable_limit_ == status;
 }
 
-template <typename T> inline bool PidPosition<T>::SetLimits(T min, T max) {
+template <typename T> inline bool PositionalPid<T>::SetLimits(T min, T max) {
   limit_min_ = min;
   limit_max_ = max;
   return true;
 }
 
-template <typename T> T PidPosition<T>::Proportional() { return e_; }
+template <typename T> T PositionalPid<T>::Proportional() { return e_; }
 
-template <typename T> T PidPosition<T>::Integral() {
+template <typename T> T PositionalPid<T>::Integral() {
   return integral_0_t_ += e_;
 }
 
-template <typename T> T PidPosition<T>::Derivative() {
-  auto diff = e_ - last_e_;
-  last_e_ = e_;
-  return diff;
-}
+template <typename T> T PositionalPid<T>::Derivative() { return e_ - last_e_; }
 
 template <typename T>
-T PidPosition<T>::Calculate(const T target, const T actual) {
+T PositionalPid<T>::Calculate(const T target, const T actual) {
   e_ = target - actual;
   u_ = kp_ * Proportional() + ki_ * Integral() + kd_ * Derivative();
   if (enable_limit_) {
     u_ = std::clamp(u_, limit_min_, limit_max_);
   }
+  last_e_ = e_;
   return u_;
 }
 
