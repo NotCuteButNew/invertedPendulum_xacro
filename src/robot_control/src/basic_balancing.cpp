5655eeb95832f1ed98cd019931c5a6ef1e0a2f81
NotCuteButNew <bwd2386884119@gmail.com>
2023-08-17 19:36:42 +0800

    add new pid

--- src/robot_control/src/basic_balancing.cpp
+++ src/robot_control/src/basic_balancing.cpp
@@ -1,5 +1,6 @@
-#include "pid.h"
 #include "config.h"
+#include "pid.h"
+#include "pid_position.hpp"
 #include "ros/init.h"
 #include "ros/node_handle.h"
 #include "ros/publisher.h"
@@ -14,19 +15,20 @@ ros::Publisher pub;
 std_msgs::Float64 msg;
 pid::PositionalPid<float> pid_rad(KP_RAD, KI_RAD, KD_RAD);
 pid::IncrementalPid<float> pid_vel(KP_VEL, KI_VEL, KD_VEL);
+pid::PositionalPid<float> pid_block_pos(KP_BLOCK_POS, KI_BLOCK_POS,
+                                        KD_BLOCK_POS);
 
 void DoMsg(sensor_msgs::JointState::ConstPtr ptr) {
-  float force_pos=0,force_rad=0;
-  if (ptr->position[0] > MIN_RAD && ptr->position[0] < MAX_RAD)
-    {
-      force_rad=pid_rad.Calculate(0, ptr->position[0]);
-      force_pos=pid_vel.Calculate(force_rad,ptr->velocity[0]);
-      msg.data=force_rad;
-    }
-  else {
+  float force_vel = 0, force_rad = 0, force_block_pos = 0;
+  if (ptr->position[0] > MIN_RAD && ptr->position[0] < MAX_RAD) {
+    force_rad = pid_rad.Calculate(0, ptr->position[0]);
+    force_vel = pid_vel.Calculate(force_rad, ptr->velocity[0]);
+    force_block_pos = pid_block_pos.Calculate(0, ptr->position[1]);
+    msg.data = force_vel;
+  } else {
     ROS_INFO("Out Of Range!!!");
-    force_pos=pid_vel.Calculate(0,ptr->position[1]);
-    msg.data=force_pos;
+    force_vel = pid_vel.Calculate(0, ptr->position[1]);
+    msg.data = force_vel;
   }
   pub.publish(msg);
   ROS_INFO("force:%.2f", msg.data);
@@ -37,7 +39,7 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "balance");
   ros::NodeHandle pub_joint1, sub_joint2;
   pub = pub_joint1.advertise<std_msgs::Float64>(
-    "/inverted_pendulum/joint1_effort_controller/command", 10);
+      "/inverted_pendulum/joint1_effort_controller/command", 10);
   ros::Subscriber sub = sub_joint2.subscribe<sensor_msgs::JointState>(
       "/inverted_pendulum/joint_states", 10, DoMsg);
   ros::spin();
